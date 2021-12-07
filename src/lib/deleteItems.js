4562cb0e65fc309560d61c7fb904c64269444f10
Marcelo <marcelo.kovalink@gmail.com>
2021-12-06 23:37:23 -0300

    Moved redirect to end of promise

--- src/lib/deleteItems.js
+++ src/lib/deleteItems.js
@@ -1,9 +1,18 @@
 import { showingIds } from '../showingIds';
 
-function deleteItems() {   
-    showingIds.forEach(el => {
-        deleteItemFromDb(`https://scandiappdb.herokuapp.com/api/public/delete.php?id=${el}`);
+function deleteItems() {
+    
+    let promiseArr = showingIds.map(function (el) {
+        return deleteItemFromDb(`https://scandiappdb.herokuapp.com/api/public/delete.php?id=${el}`).then(function (res) {
+            return res;
+        })
     });
+    
+    Promise.all(promiseArr).then(function(){
+        window.location.assign('/');
+    }).catch(function(err){
+        console.log(err);
+    })
 }
 
 async function deleteItemFromDb(url) {
@@ -14,8 +23,6 @@ async function deleteItemFromDb(url) {
                                         'Content-type': 'application/json'
                                     }
                                 });
-                            
-        
         console.log(deleted);
     } catch (error) {
         console.log(error);
