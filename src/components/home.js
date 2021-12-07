1acc62a5dce46d521537cc600cc19d34d1dcc0ca
Marcelo <marcelo.kovalink@gmail.com>
2021-12-06 23:41:32 -0300

    moved redirect call to deleteItems

--- src/components/home.js
+++ src/components/home.js
@@ -4,31 +4,33 @@ import deleteItems from '../lib/deleteItems';
 import Footer from './footer';
 
 class Home extends React.Component {
+      constructor(props) {
+      super(props);
+      this.performMassDelete = this.performMassDelete.bind(this);
+    }
       
-      performMassDelete() {
+    performMassDelete() {
         deleteItems();
-        window.location.assign('/');
-      }
+    }
     
-      render() {
+    render() {
         return (
-          <div className="App">
-              <header className="App-header">
-                <nav className="topNavigation">
-                  <h1>Product List</h1>
-                    <ul className="nav-btns">
-                        <li className="btn"><button><a href="/addproduct">ADD</a></button></li>
-                        <li className="btn"><button id="delete-product-btn" onClick={this.performMassDelete}>MASS DELETE</button></li>
-                    </ul>
-                </nav>
-              </header>
+            <div className="App">
+                <header className="App-header">
+                  <nav className="topNavigation">
+                    <h1>Product List</h1>
+                      <ul className="nav-btns">
+                          <li className="btn"><button><a href="/addproduct">ADD</a></button></li>
+                          <li className="btn"><button id="delete-product-btn" onClick={this.performMassDelete}>MASS DELETE</button></li>
+                      </ul>
+                  </nav>
+                </header>
           
-              <ProductList />
+                <ProductList />
         
-              <Footer />
-          </div>
+                <Footer />
+            </div>
         );      
-      }
+    }
 }
 export default Home;
-      
